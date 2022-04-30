8642340da876ca28a7e5e2b413182a771df612ea
Arimardan Yadav <arimardan.yadav@watchguard.com>
2022-04-30 13:32:30 +0530

    Solution for LeetCode 743 - Network Delay Time

--- LeetCode/src/743 - Network Delay Time.cpp
+++ LeetCode/src/743 - Network Delay Time.cpp
@@ -1,13 +1,14 @@
 /*
 Problem Name: Network Delay Time
 Problem Link: https://leetcode.com/problems/network-delay-time/
-Tag: Graph, Dijkastra Algo
+Tag: Graph, Dijkastra Algo, Bellman Ford Algo
 Concept: Use Dijkastra Algo for weighted directed graph to find the shortest distance to farthest node
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Dijkastra
 class Solution {
    public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
@@ -42,6 +43,44 @@ class Solution {
         int res = 0;
         for (int i = 1; i < dist.size(); i++) {
             if (dist[i] == INT_MAX) return -1;
+            res = max(res, dist[i]);
+        }
+        return res;
+    }
+};
+
+// Bellman Ford
+class Solution {
+   public:
+    struct edge {
+        int u, v, wt;
+        edge(int first, int second, int third) {
+            u = first;
+            v = second;
+            wt = third;
+        }
+    };
+
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<edge> graph;
+        for (auto it : times) {
+            graph.push_back(edge(it[0], it[1], it[2]));
+        }
+
+        vector<int> dist(n + 1, 1e9);
+        dist[k] = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (auto it : graph) {
+                if (dist[it.v] > dist[it.u] + it.wt) {
+                    dist[it.v] = dist[it.u] + it.wt;
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == 1e9) return -1;
             res = max(res, dist[i]);
         }
         return res;
